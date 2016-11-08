02a29fd352e4fc3c69c196b43924a87a09796423
Ian Patel <ian.patel@yahoo.com>
2016-11-08 16:40:38 -0500

    Convert to binary strings in an attempt to add python3 support

--- LMS_tests/LMS_setup.py
+++ LMS_tests/LMS_setup.py
@@ -47,34 +47,34 @@ n = 0
 
 while config==0:
     response=ser.read()
-    if binascii.hexlify(response)=='':
+    if binascii.hexlify(response)==b'':
         print('. . .')
         count += 1
     if count == 5:
         ser.write(serial.to_bytes([0x02,0x00,0x01,0x00,0x31,0x15,0x12])) # Status command
         count = 0
 
-    if binascii.hexlify(response)=='06':
+    if binascii.hexlify(response)==b'06':
         time1 = time.time()
         print('Command Acknowledged')
 
-    if binascii.hexlify(response)=='02':
+    if binascii.hexlify(response)==b'02':
         response=ser.read()
-        if binascii.hexlify(response)=='80':
+        if binascii.hexlify(response)==b'80':
             response=ser.readline()
             len = int(binascii.hexlify(response[1]+response[0]), 16)
-            #print('Length: ' + str(len))
+            print('Length: ' + str(len))
             #print('Response: ' + binascii.hexlify(response[2]))
             #print('Status: ' + binascii.hexlify(response[len+1]))
             #print('Data: ' + binascii.hexlify(response[3:len+1]))
 
-            if binascii.hexlify(response[2])=='90':
+            if binascii.hexlify(response[2])==b'90':
                 print('LMS291 Powered On')
                 #ser.write(serial.to_bytes([0x02,0x00,0x0A,0x00,0x20,0x00,0x53,0x49,0x43,0x4B,0x5F,0x4C,0x4D,0x53,0xBE,0xC5])) # Settings mode
                 ser.write(serial.to_bytes([0x02,0x00,0x05,0x00,0x3B,0xB4,0x00,0x64,0x00,0x97,0x49])) # 180-1 degree variant
                 #config = 1
 
-            elif binascii.hexlify(response[2])=='b1':
+            elif binascii.hexlify(response[2])==b'b1':
                 print('LMS291 Connection Status Verified: ' + response[3:10])
                 #ser.write(serial.to_bytes([0x02,0x00,0x0A,0x00,0x20,0x00,0x53,0x49,0x43,0x4B,0x5F,0x4C,0x4D,0x53,0xBE,0xC5])) # Settings mode
 
@@ -87,13 +87,13 @@ while config==0:
 
                 #config = 1
 
-            elif binascii.hexlify(response[2])=='a0':
+            elif binascii.hexlify(response[2])==b'a0':
                 print('LMS291 Installation Mode')
                 #ser.write(serial.to_bytes([0x02,0x00,0x02,0x00,0x20,0x40,0x50,0x08])) # Switch to 38,400 Bd
                 #config = 1
-            elif binascii.hexlify(response[2])=='bb':
+            elif binascii.hexlify(response[2])==b'bb':
                 print('LMS291 Variant Switch')
-                if binascii.hexlify(response[3])=='01':
+                if binascii.hexlify(response[3])==b'01':
                     print('Switchover successful: ' + str(int(binascii.hexlify(response[4]),16)) + ' by ' + str(int(binascii.hexlify(response[6]),16)/100) + ' degrees')
                     ang_start = (180-int(binascii.hexlify(response[4]),16))/2
                     ang_inc = int(binascii.hexlify(response[6]),16)/100
@@ -105,9 +105,9 @@ while config == 1:
     #ser.write(serial.to_bytes([0x02,0x00,0x02,0x00,0x36,0x02,0x3E,0x1E])) # Mean Measured Data
     ser.write(serial.to_bytes([0x02,0x00,0x02,0x00,0x30,0x01,0x31,0x18])) # Single Scan
     response=ser.read()
-    if binascii.hexlify(response)=='02':
+    if binascii.hexlify(response)==b'02':
         response=ser.read()
-        if binascii.hexlify(response)=='80':
+        if binascii.hexlify(response)==b'80':
             len_low=ser.read()
             len_high=ser.read()
             len = int(binascii.hexlify(len_high+len_low), 16)
